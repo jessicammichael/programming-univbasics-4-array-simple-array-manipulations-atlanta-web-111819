04098624536e4d080ab1aa77dea3a4b9357887f8
Jessica Michael <pantherjmm@yahoo.com>
2019-11-15 04:00:15 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,17 @@
+'''ruby
+
+friends = ["Jerry", "Larry", "Bob"]
+next_friend = "Sam"
+friends.push(next_friend)
+
+friends = ["Jerry", "Larry", "Bob", "Sam"]
+next_friend = "Guy"
+friends.unshift(next_friend)
+
+friends = ["Guy","Jerry", "Larry", "Bob", "Sam"]
+sam_friends = friends.pop
+
+friends = ["Guy","Jerry", "Larry", "Bob"]
+friends.pop ("Bob", "Larry")
+
+friends = ["Jerry"]
